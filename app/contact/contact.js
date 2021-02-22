7dd2862447d2ad57c3e6f3d64b7a9670e9cfeb4a
cyau60379 <cyril.aubourg@isep.fr>
2021-02-22 18:46:00 +0100

    finish contact form system + add animations

--- app/contact/contact.js
+++ app/contact/contact.js
@@ -2,44 +2,17 @@
 
 angular.module('myApp.contact', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/contact', {
-    templateUrl: 'contact/contact.html',
-    controller: 'contactCtrl'
-  });
-}])
+    .config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.when('/contact', {
+            templateUrl: 'contact/contact.html',
+            controller: 'contactCtrl'
+        });
+    }])
 
-.controller('contactCtrl', function($scope) {
-    $scope.formData = {};
+    .controller('contactCtrl', function ($scope, $window) {
+        $scope.formData = {};
 
-    $scope.sendMail = function () {
-        try {
-            var nodemailer = require('nodemailer');
-
-            var transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: 'service.contact.vas@gmail.com',
-                    pass: 'V1t0r14G4st31z'
-                }
-            });
-
-            var mailOptions = {
-                from: 'service.contact.vas@gmail.com',
-                to: 'service.contact.vas@gmail.com',
-                subject: $scope.formData.name + " - " + $scope.formData.subject,
-                text: $scope.formData.main
-            };
-
-            transporter.sendMail(mailOptions, function(error, info){
-                if (error) {
-                    console.log(error);
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
-        } catch (e) {
-            alert("No server found");
-        }
-    };
-});
+        $scope.sendMail = function () {
+            $window.open("mailto:service.contact.vas@gmail.com?subject=" + $scope.formData.name + " - " + $scope.formData.subject + "&body=" + $scope.formData.main, "_self");
+        };
+    });
